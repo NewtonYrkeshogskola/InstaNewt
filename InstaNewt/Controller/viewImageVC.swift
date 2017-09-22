bfa2f015f563b2e315e1580c941de8da6bcf97e7
Giovanni Palusa <giovanni@palusa.se>
2017-09-22 11:32:56 +0200

    added like button to view

--- InstaNewt/Controller/viewImageVC.swift
+++ InstaNewt/Controller/viewImageVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class viewImageVC: UIViewController {
 
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postText: UILabel!
+    @IBOutlet weak var likeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +25,5 @@ class viewImageVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
