6f2ccd99cdcf8fd05c36406f2dc49be9078f9fdf
JacobAhlberg <jacahlberg@hotmail.com>
2017-09-22 11:28:22 +0200

    Added Views for the postCell and a model for the cell

---
+++ InstaNewt/Views/PostCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostCell.swift
+//  InstaNewt
+//
+//  Created by Jacob Ahlberg on 2017-09-22.
+//  Copyright © 2017 Jacob Ahlberg. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // WE NEED TO UPDATE THE IMAGEVIEW WITH THE DATA
+    func updateCells(){
+       
+    }
+    
+}
