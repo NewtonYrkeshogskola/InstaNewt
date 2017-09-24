0f1d537ef9a1434909b093fa150a3ca3f961523d
Giovanni Palusa <giovanni@palusa.se>
2017-09-24 22:07:29 +0200

    added example data and images

--- InstaNewt/Controller/MainViewController.swift
+++ InstaNewt/Controller/MainViewController.swift
@@ -17,6 +17,25 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         imageCollectionView.delegate = self
         imageCollectionView.dataSource = self
+        
+        // Adding example data
+        
+        let blobb4: NSData = UIImagePNGRepresentation(UIImage(named: "flowers")!)! as NSData
+        let postobj4 = Post(favourite: true, imageText: "Oh look at deez flowers!!!!!!11!!" ,imageData: blobb4)
+        imageArray.append(postobj4)
+        
+        let blobb: NSData = UIImagePNGRepresentation(UIImage(named: "rainbow")!)! as NSData
+        let postobj = Post(favourite: true, imageText: "Rainbows are beautiful" ,imageData: blobb)
+        imageArray.append(postobj)
+        
+        let blobb2: NSData = UIImagePNGRepresentation(UIImage(named: "water")!)! as NSData
+        let postobj2 = Post(favourite: false, imageText: "Summer is awesome!" ,imageData: blobb2)
+        imageArray.append(postobj2)
+        
+        let blobb3: NSData = UIImagePNGRepresentation(UIImage(named: "turtle")!)! as NSData
+        let postobj3 = Post(favourite: false, imageText: "Turtles are reptiles of the order Testudines characterised by a special bony or cartilaginous shell developed from their ribs and acting as a shield. \"Turtle\" may refer to the order as a whole or to fresh-water and sea-dwelling testudines." ,imageData: blobb3)
+        imageArray.append(postobj3)
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -36,7 +55,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
             self.imageCollectionView.reloadData()
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
