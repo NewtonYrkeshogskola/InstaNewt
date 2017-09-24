cf800d03801b12124022269e0e197b5be3dc291b
Giovanni Palusa <giovanni@palusa.se>
2017-09-24 20:37:20 +0200

    minor design changes

---
+++ InstaNewt/Controller/ViewPostVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewPostVC.swift
+//  InstaNewt
+//
+//  Created by Giovanni Palusa on 2017-09-24.
+//  Copyright © 2017 Jacob Ahlberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewPostVC: UIViewController {
+
+    @IBOutlet weak var blurredImg: UIImageView!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var mainLbl: UILabel!
+    @IBOutlet weak var likeBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func likeAcn(_ sender: Any) {
+    }
+    
+
+}
